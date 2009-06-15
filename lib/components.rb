33886bcf73ea0cfa9a1621d8f31b55fc0ec87696
Nicolas Berger <nicoberger@gmail.com>
2009-06-15 03:14:21 -0300

    render_component patch. flash was not working

--- lib/components.rb
+++ lib/components.rb
@@ -136,7 +136,7 @@ module Components
       def assign_shortcuts_with_render_component(request, response)
         assign_shortcuts_without_render_component(request, response)
         flash(:refresh)
-        flash.sweep if @_session && !component_request?
+        #flash.sweep if @_session && !component_request?
       end
   end
 end
